bc1f0a5234d68bcb2130c8bfd0aeea45fabae0b6
suvanamruth <samruth@ucsc.edu>
2024-06-24 23:15:11 -0700

    testing forest model on 2023 season stats. Working model but has many outliers

--- mvpfiles/getStats.py
+++ mvpfiles/getStats.py
@@ -9,12 +9,6 @@ import os
 
 # Output all player season totals from each season into a csv file stored in statsBySeason folder 
 """
-print("Writing player season totals for each season to CSV file")
-for i in range(1987, 2024):
-    file_path = "./statsBySeason/" + str(i) + "_" + str(i+1) + "_player_season_totals.csv"
-    client.players_season_totals(season_end_year=i+1, output_type=OutputType.CSV, output_file_path=file_path)
-"""
-
 for i in range(2017, 2024):
     source = requests.get(f"https://www.basketball-reference.com/awards/awards_{i+1}.html").text
     soup = BeautifulSoup(source, 'html.parser')
@@ -23,3 +17,19 @@ for i in range(2017, 2024):
     df = pd.read_html(str(table))[0]
     df.to_csv(f"./trainData/{i+1}mvpVotes.csv")
 
+
+for i in range(2013, 2024):
+    source = requests.get(f"https://www.basketball-reference.com/leagues/NBA_{i+1}_per_game.html").text
+    soup = BeautifulSoup(source, 'html.parser')
+    table2 = soup.find('table', attrs={'id': 'per_game_stats'})
+    df2 = pd.read_html(str(table2))[0]
+    df2.to_csv(f"./pgstats/{i+1}perGameStats.csv")
+"""
+
+for i in range(2013, 2024):
+    source = requests.get(f"https://www.basketball-reference.com/leagues/NBA_{i+1}_advanced.html").text
+    soup = BeautifulSoup(source, 'html.parser')
+    table2 = soup.find('table', attrs={'id': 'advanced_stats'})
+    df2 = pd.read_html(str(table2))[0]
+    df2 = df2[['Player', 'WS', 'WS/48']]
+    df2.to_csv(f"/Users/suvanamruth/Documents/MVPprediction/mvpfiles/winShares/{i+1}winShares.csv")
